214e92369e41dcd1823268a9920f27cfd8b13711
Lucas Fernandes <lucasfern99@gmail.com>
2021-11-01 14:17:36 -0300

    1.0.0 stable

---
+++ README.md
@@ -0,0 +1,6 @@
+# FileOrganizer
+A python3 program that organizes all the files by format in a given folder.
+
+# Instructions
+1- clone the repository
+2- run main.py
--- main.py
+++ main.py
@@ -1,26 +1,6 @@
-import conf
+import app
 
-<<<<<<< HEAD
-assert ('linux' in sys.platform), 'This program runs on Linux only'
-while True:
-    cf.run()
-
-    fo.run2()
-    if not cf.YorN('\n'+str(fo.filesFound)+' files with matching formats found. Proceed?'):
-        print('Restarting...')
-        time.sleep(1)
-        continue
-    
-    fo.runFinal()
-    break
-=======
 def main():
-    app = conf.App()
-    if not app.checkSudo():
-        return
-
-    app.start()
-
-
+	file_organizer = app.App()
+	file_organizer.run()
 main()
->>>>>>> 27398bc0533806e0b791822b5204dc1239ed0945
